daca1666164774027505afedd46039e08e830056
devexcelsior <40436735+devexcelsior@users.noreply.github.com>
2024-04-14 23:21:08 -0400

    Helios Design System Integration, switch to scss.

--- packages/frontend/ember-cli-build.js
+++ packages/frontend/ember-cli-build.js
@@ -5,8 +5,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
-
-    // Add options here
+    sassOptions: {
+      precision: 4,
+      includePaths: [
+        './node_modules/@hashicorp/design-system-tokens/dist/products/css',
+        './node_modules/@hashicorp/ember-flight-icons/dist/styles',
+        './node_modules/@hashicorp/design-system-components/dist/styles',
+      ],
+    },
   });
 
   return app.toTree();
